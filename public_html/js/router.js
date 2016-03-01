c67fd07aa55da948a7583147dd2d55720e1a089b
Alexey <avtyul@github.com>
2016-03-01 12:07:12 +0300

    lets try materialize + some fix

--- public_html/js/router.js
+++ public_html/js/router.js
@@ -1,29 +1,26 @@
-define([
-    'backbone'
-], function(
-    Backbone
-){
+define(
+    ['backbone', 'views/main', 'views/scoreboard', 'views/game', 'views/login', 'views/register'],
+    function (Backbone) {
+        var Router = Backbone.Router.extend({
+            routes: {
+                'scoreboard': 'scoreboardAction',
+                'game': 'gameAction',
+                'login': 'loginAction',
+                '*default': 'defaultActions'
+            },
+            defaultActions: function () {
+                // TODO
+            },
+            scoreboardAction: function () {
+                // TODO
+            },
+            gameAction: function () {
+                // TODO
+            },
+            loginAction: function () {
+                // TODO
+            }
+        });
 
-    var Router = Backbone.Router.extend({
-        routes: {
-            'scoreboard': 'scoreboardAction',
-            'game': 'gameAction',
-            'login': 'loginAction',
-            '*default': 'defaultActions'
-        },
-        defaultActions: function () {
-            // TODO
-        },
-        scoreboardAction: function () {
-            // TODO
-        },
-        gameAction: function () {
-            // TODO
-        },
-        loginAction: function () {
-            // TODO
-        }
+        return new Router();
     });
-
-    return new Router();
-});
