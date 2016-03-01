f47e1832ffba04bd9ba94799de8dab52f53f225e
Alexey <avtyul@github.com>
2016-03-01 14:31:47 +0300

    add config

---
+++ public_html/js/configuration.js
@@ -0,0 +1,19 @@
+require.config({
+    urlArgs: "_=" + (new Date()).getTime(),
+    baseUrl: "js",
+    paths: {
+        jquery: "lib/jquery",
+        underscore: "lib/underscore",
+        backbone: "lib/backbone",
+        materialize: 'lib/materialize'
+    },
+    shim: {
+        'backbone': {
+            deps: ['underscore', 'jquery'],
+            exports: 'Backbone'
+        },
+        'underscore': {
+            exports: '_'
+        }
+    }
+});
--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,24 +1,4 @@
-require.config({
-    urlArgs: "_=" + (new Date()).getTime(),
-    baseUrl: "js",
-    paths: {
-        jquery: "lib/jquery",
-        underscore: "lib/underscore",
-        backbone: "lib/backbone",
-        materialize: 'lib/materialize'
-    },
-    shim: {
-        'backbone': {
-            deps: ['underscore', 'jquery'],
-            exports: 'Backbone'
-        },
-        'underscore': {
-            exports: '_'
-        }
-    }
-});
-
-require([
+define([
     'backbone',
     'router'
 ], function(
