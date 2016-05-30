3b28c8e1674064b872b4c274424c972155bc376b
Alexey <avtyul@github.com>
2016-05-30 20:47:02 +0300

    awesome preloader

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,8 +1,15 @@
-define( function(require) {
-    
+define(function (require) {
     var Backbone = require('backbone'),
-        router = require('router');
-	    
-	    Backbone.history.start();
-	}
-);
+        router = require('router'),
+        $ = require('jquery');
+
+    Backbone.history.start();
+
+    var removePreloader = function() {
+        $("#preloader").fadeOut('slow');
+        $("#page").fadeIn('slow');
+        document.getElementById('page').style.display = 'block';
+    };
+
+    setTimeout(removePreloader, 5000);
+});
