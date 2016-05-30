e3e4f01d266a57b03da4784890c90dda4e4c51f4
Alexey <avtyul@github.com>
2016-05-30 23:57:07 +0300

    ws

--- public_html/js/views/game.js
+++ public_html/js/views/game.js
@@ -3,18 +3,21 @@ define(function (require) {
     var Backbone = require('backbone'),
         BaseView = require('views/base'),
         tmpl = require('tmpl/game'),
-        createjs = require('easel'),
+        $ = require('jquery'),
+        alertify = require('alertify'),
         MainSquareView = require('views/mainSquare'),
         MainSquareModel = require('models/mainSquare'),
-        SinglePlayerModel = require('models/singlePlayer'),
         user = require('models/user');
 
     var GameView = BaseView.extend({
         initialize: function () {
             this.$page = $('#page');
             this.$page.append(this.el);
+            // this.mainSquareView = new MainSquareView;
+            // this.mainSquareModel = new MainSquareModel;
             this.render();
             this.hide();
+            this.websocket = null;
         },
 
         template: tmpl,
@@ -22,22 +25,54 @@ define(function (require) {
         show: function () {
             this.$el.appendTo("#page");
             this.$el.show();
+            $("#preloader").fadeIn('slow');
+            $("#page").fadeOut('slow');
+            document.getElementById('page').style.display = 'none';
             this.trigger('show', this);
             if(user.get('isAuth') === false) {
                 Backbone.history.navigate('#', true);
             }
-            if(!this.websocket) {
-                if(this.websocket.readyState === 3)
-                {
-                    this.websocket = new WebSocket("ws://127.0.0.1/game");
-                }
-            }
+            this.websocket = new WebSocket("ws://127.0.0.1:8090/game");
+            this.websocket.onmessage = this.handleMessage.bind(this);
         },
         
         hide: function() {
             this.$el.hide();
             if(this.websocket && this.websocket.readyState < 2) {
                 this.websocket.close();
+            }
+        },
+
+        sendSqr: function(msg) {
+            if(this.websocket && this.websocket.readyState < 2) {
+                this.websocket.send(msg);
+            }
+        },
+
+        handleMessage: function (msg) {
+            var resp = JSON.parse(msg.data);
+            console.log(resp);
+            switch(resp.status) {
+                case "START_GAME":
+                    $("#preloader").fadeOut('slow');
+                    $("#page").fadeIn('slow');
+                    document.getElementById('page').style.display = 'block';
+                    alertify.alert('Tic tac toe', 'Your opponent: ' + resp.opponentName);
+                    break;
+                case "OPPONENT_DISCONNECT": {
+                    alertify.alert('Tic tac toe', 'Opponent disconected');
+                    Backbone.history.navigate('#menu', true);
+                    break;
+                }
+            }
+            if(resp.map && resp.map[0] !== null) {
+                for(var i = 0; i < resp.map.length; i++) {
+                    var parent, child, playerId,
+                        splitted = resp.map[i].split(".");
+                    parent = splitted[0];
+                    child = splitted[1];
+                    playerId = splitted[2];
+                }
             }
         }
     });
